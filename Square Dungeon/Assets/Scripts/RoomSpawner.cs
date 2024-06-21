e91289e8f7933054e905d6f6865815be9988e743
AlienMan17 <10931268@uvu.edu>
2024-06-21 00:08:27 -0600

    Added room generation
    it is buggy and doesn't work completely yet, there is just a bug, and it runs indefinitely.

---
+++ Square Dungeon/Assets/Scripts/RoomSpawner.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+    public int openingDirection;
+    private RoomTemplates roomTemplates;
+    int rand;
+    private bool spawned = false;
+
+    void Start()
+    {
+        roomTemplates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+        Invoke("Spawn", 0.1f);
+    }
+
+    // Update is called once per frame
+    void Spawn()
+    {
+        if (spawned == false)
+        {
+            if (openingDirection == 1)
+            {
+                rand = Random.Range(0, roomTemplates.bottomRooms.Length);
+                Instantiate(roomTemplates.bottomRooms[rand], transform.position, roomTemplates.bottomRooms[rand].transform.rotation);
+                //needs to generate a room with a BOTTOM door
+            }
+            else if (openingDirection == 2)
+            {
+                rand = Random.Range(0, roomTemplates.topRooms.Length);
+                Instantiate(roomTemplates.topRooms[rand], transform.position, roomTemplates.topRooms[rand].transform.rotation);
+                //needs to generate a room with a TOP door
+            }
+            else if (openingDirection == 3)
+            {
+                rand = Random.Range(0, roomTemplates.leftRooms.Length);
+                Instantiate(roomTemplates.leftRooms[rand], transform.position, roomTemplates.leftRooms[rand].transform.rotation);
+                //needs to generate a room with a LEFT door
+            }
+            else if (openingDirection == 4)
+            {
+                rand = Random.Range(0, roomTemplates.rightRooms.Length);
+                Instantiate(roomTemplates.rightRooms[rand], transform.position, roomTemplates.rightRooms[rand].transform.rotation);
+                //needs to generate a room with a RIGHT door
+            }
+            spawned = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("SpawnPoint") && other.GetComponent<RoomSpawner>().spawned)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
